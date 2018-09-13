219769f4ccbd052783111945be82b52840d0faad
Computer <computer@Computers-MacBook-Air.local>
2018-09-13 16:53:56 -0700

    added redux and created a components subfolder, changed routes, imported people.json file, created images, reducers, actions folders

--- crm/components/App.js
+++ crm/components/App.js
@@ -9,10 +9,14 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import firebase from 'firebase';
-import Test from './Test';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import Login from './Login';
 import Loader from './Loader';
 import PeopleList from './PeopleList';
+import reducers from '../reducers/PeopleReducer';
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -49,9 +53,11 @@ export default class App extends Component<Props> {
 
   render() {
     return (
-      <View style={styles.container}>
-        {this.renderInitialView()}
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {this.renderInitialView()}
+        </View>
+      </Provider>
     );
   }
 }
---
+++ crm/components/PeopleItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { connect } from 'react-redux';
+import { getTheme } from 'react-native-material-kit';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import * as actions from '../actions';
+
+const theme =getTheme();
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: 20,
+  },
+  title: {
+    top: 20,
+    left: 80,
+    fontSize: 24,
+  },
+  image: {
+    height: 100,
+  },
+  action: {
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  icon: {
+    position: 'absolute',
+    top: 15,
+    left: 15,
+    color: 'white',
+    backgroundColor: 'rgba(255,255,255,0)',
+  },
+});
+
+const PeopleItem = (props) => {
+  return(
+    <View style={[theme.cardStyle, styles.card]}>
+      <Image
+        source={{uri: '/Users/computer/projects/react_native_app/crm/images/background.jpg'}}
+        style={[theme.cardImageStyle, styles.image]}/>
+      <Icon
+        name={'user'}
+        size={100}
+        style={styles.icon}/>
+      <Text style={[theme.cardtTitleStyle, styles.title]}>{props.people.first_name}{props.people.last_name}</Text>
+      <Text style={[theme.cardActionStyle, styles.action]}>{props.people.company}</Text>
+    </View>
+  );
+};
+
+export default connect(null, actions)(PeopleItem);
